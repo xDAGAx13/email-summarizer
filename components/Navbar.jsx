e77463af28697083a01049c7fa7d191116121822
xDAGAx13 <rohandaga05@gmail.com>
2025-07-09 15:45:31 +0530

    added packages

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,8 @@
 'use client'
-import { useRouter } from 'next/navigation'
 import React from 'react'
 import Link from 'next/link'
 
 const Navbar = () => {
-  const router = useRouter();
   return (
     <nav className='flex justify-between px-5 lg:px-20 py-5 mb-10'>
       <h1 className='nav-item'>EM-Sum</h1>
