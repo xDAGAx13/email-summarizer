6bbf406cbba81ef8540081599826679a84110b86
xDAGAx13 <rohandaga05@gmail.com>
2025-07-12 14:58:10 +0530

    added some stuff

--- app/Auth/page.jsx
+++ app/Auth/page.jsx
@@ -1,15 +1,16 @@
-import Navbar from '../../components/Navbar'
-import React from 'react'
-import Login from './sections/Login'
+"use client";
+import Navbar from "../../components/Navbar";
+import React from "react";
+import Login from "./sections/Login";
 
 const signin = () => {
   return (
     <div>
-    <div className='background'></div>
-      <Navbar/>
-      <Login/>
+      <div className="background"></div>
+      <Navbar />
+      <Login />
     </div>
-  )
-}
+  );
+};
 
-export default signin
+export default signin;
